13f09d70e331d43a8e33cf5f13b730a3f75846a3
ManasesHerrera <manases.herrera@emilfriedman.con.ve>
2020-11-23 12:30:20 -0400

    mi primer subida

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+	<div class="container">
+		<div class="c-video">
+			<video class="video" src="video/video.mp4"  ></video>
+			<div class="controls">
+				<div class="color-bar">
+					 <div class="color-juice"> </div>
+				</div>
+				 <div class="buttons">
+				 	<button type="button" id="playPause"></button>
+				 </div> 
+			</div>
+		</div>
+	</div>
+
+	
+	<script src="js/jquery.js"></script>
+	<script src="js/script.js"></script>
+</body>
+</html>			
---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+// function togglePlayPause(){
+// 	if (video.paused) {
+// 		btn.className = 'pause';
+// 		video.play;
+// 	}else{
+// 		btn.className = "play";
+// 		video.paused;
+// 	}
+// }
+
+// btn.onclick = function (){
+// 	alert("play");
+// }
+
+
+
+$(document).ready(function(){
+	var video = document.querySelector('.video');
+	var juice = document.querySelector('.color-juice');
+	var btn = document.getElementById('playPause');
+
+	// var video = $(".video");
+	// var juice = $(".color-juice");
+	// var btn = $("#play-pause");
+
+	$("#playPause").click(function(){
+		
+
+
+		if (video.paused) {
+			btn.className = 'pause';
+			video.play();
+		}else{
+			btn.className = "play";
+			video.pause();
+		}
+	});
+
+
+});
